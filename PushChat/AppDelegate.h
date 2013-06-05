56f5f3eea0d2b28a720b875ae9bc4869ab435457
edwardean <lihangqw@gmail.com>
2013-06-05 20:36:14 +0800

    init

---
+++ PushChat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PushChat
+//
+//  Created by Edward on 13-6-5.
+//  Copyright (c) 2013年 Lihang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PushChat/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PushChat
+//
+//  Created by Edward on 13-6-5.
+//  Copyright (c) 2013年 Lihang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
